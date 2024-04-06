ab796de098fdb99f7885a0c904e73a7e76a084f4
James Martin <jamesm@pololu.com>
2024-04-05 17:22:23 -0700

    add config

--- tmux/default.nix
+++ tmux/default.nix
@@ -3,7 +3,7 @@
 {
   programs.tmux = {
     enable = true;
-    extraConfig = builtins.readFile ./tmux.conf; # for now diabling this and even using the same config has some issues with escape being sent to tmux to switch windows 
+    extraConfig = builtins.readFile ./tmux.conf; # for now diabling this and even using the same config ... was having some issues with escape being sent to tmux to switch windows  (like an escape delay that was being ignored) 
     disableConfirmationPrompt = true;
     historyLimit = 100000;
     newSession = false;
